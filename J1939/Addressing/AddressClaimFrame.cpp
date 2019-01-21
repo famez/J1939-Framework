f434d53ee842363342c78a12d72f11b2a1bf954c
famez <f.amez1992@gmail.com>
2019-01-21 18:52:07 -0500

    Fixed misspelling in file

--- J1939/Addressing/AddressClaimFrame.cpp
+++ J1939/Addressing/AddressClaimFrame.cpp
@@ -9,98 +9,95 @@
 #include <sstream>
 #include <iostream>
 
-#include <Addressing/AdressClaimFrame.h>
+#include <Addressing/AddressClaimFrame.h>
 
 #define ADDRESS_CLAIM_NAME		"Address Claim"
 
 namespace J1939 {
 
-AdressClaimFrame::AdressClaimFrame() : J1939Frame(ADDRESS_CLAIM_PGN) {
+AddressClaimFrame::AddressClaimFrame() : J1939Frame(ADDRESS_CLAIM_PGN) {
 		mName = ADDRESS_CLAIM_NAME;
 
 }
 
-AdressClaimFrame::AdressClaimFrame(u32 idNumber, u16 manufacturerCode, u8 ecuInstance,
-			u8 functionInstance, u8 function, u8 vehicleSystem,
-			u8 vehicSystemInstance, u8 industryGroup,
-			bool arbitraryAddressCapable) : J1939Frame(ADDRESS_CLAIM_PGN), mIdNumber(idNumber), mManufacturerCode(manufacturerCode),
-					mEcuInstance(ecuInstance), mFunctionInstance(functionInstance), mFunction(function),
-					mVehicleSystem(vehicleSystem), mVehicleSystemInstance(vehicSystemInstance), mIndustryGroup(industryGroup),
-					mArbitraryAddressCapable(arbitraryAddressCapable) {
+AddressClaimFrame::AddressClaimFrame(EcuName name) : J1939Frame(ADDRESS_CLAIM_PGN), mEcuName(name) {
 
 	mName = ADDRESS_CLAIM_NAME;
 
 }
 
-AdressClaimFrame::~AdressClaimFrame() {
+AddressClaimFrame::~AddressClaimFrame() {
 }
 
 
-void AdressClaimFrame::decodeData(const u8* buffer, size_t length) {
+void AddressClaimFrame::decodeData(const u8* buffer, size_t length) {
 
 	if(length != ADDRESS_FRAME_LENGTH) {		//Check the length first
 		throw  J1939DecodeException("[AdressClaimFrame::decodeData] Buffer length does "
 				"not match the expected length. Buffer length:" + std::to_string(length) + ". Expected length: " + std::to_string(ADDRESS_FRAME_LENGTH));
 	}
 
-	mIdNumber = ( buffer[0] |
+	u32 idNumber = ( buffer[0] |
 			(buffer[1] << 8) |
 			((buffer[2] & 0x1F) << 16));
 
 
-	mManufacturerCode = (
+	u16 manufacturerCode = (
 			((buffer[2] & 0xE0) >> 5) |
 			(buffer[3] << 3)
 	);
 
-	mEcuInstance = (buffer[4] & 0x07);
+	u8 ecuInstance = (buffer[4] & 0x07);
 
-	mFunctionInstance = ((buffer[4] & 0xF8) >> 3);
+	u8 functionInstance = ((buffer[4] & 0xF8) >> 3);
 
-	mFunction = buffer[5];
+	u8 function = buffer[5];
 
-	mVehicleSystem = (buffer[6] >> 1);
+	u8 vehicleSystem = (buffer[6] >> 1);
 
-	mVehicleSystemInstance = (buffer[7] & 0x0F);
+	u8 vehicleSystemInstance = (buffer[7] & 0x0F);
 
-	mIndustryGroup = ((buffer[7] >> 4) & 0x07);
+	u8 industryGroup = ((buffer[7] >> 4) & 0x07);
 
-	mArbitraryAddressCapable = (buffer[7] >> 7);
+	bool arbitraryAddressCapable = (buffer[7] >> 7);
 
-}
-
-
-void AdressClaimFrame::encodeData(u8* buffer, size_t length) const {
-
-	buffer[0] = (mIdNumber & 0xFF);
-	buffer[1] = ((mIdNumber >> 8) & 0xFF);
-	buffer[2] = ((mIdNumber >> 16) & 0x1F) |
-			((mManufacturerCode << 5) & 0xFF);
-	buffer[3] = ((mManufacturerCode >> 3) & 0xFF);
-	buffer[4] = ((mEcuInstance & 0x07) | ((mFunctionInstance << 3) & 0xFF));
-
-	buffer[5] = mFunction;
-
-	buffer[6] = (mVehicleSystem << 1);
-
-	buffer[7] = (mVehicleSystemInstance & 4) | ((mIndustryGroup & 0x03) << 4) | ((mArbitraryAddressCapable ? 1 : 0) << 7);
+	mEcuName = EcuName(idNumber, manufacturerCode, ecuInstance, functionInstance, function,
+			vehicleSystem, vehicleSystemInstance, industryGroup, arbitraryAddressCapable);
 
 }
 
-std::string AdressClaimFrame::toString() {
+
+void AddressClaimFrame::encodeData(u8* buffer, size_t length) const {
+
+	buffer[0] = (mEcuName.getIdNumber() & 0xFF);
+	buffer[1] = ((mEcuName.getIdNumber() >> 8) & 0xFF);
+	buffer[2] = ((mEcuName.getIdNumber() >> 16) & 0x1F) |
+			((mEcuName.getManufacturerCode() << 5) & 0xFF);
+	buffer[3] = ((mEcuName.getManufacturerCode() >> 3) & 0xFF);
+	buffer[4] = ((mEcuName.getEcuInstance() & ECU_INSTANCE_MASK) | ((mEcuName.getFunctionInstance() << 3) & 0xFF));
+
+	buffer[5] = mEcuName.getFunction();
+
+	buffer[6] = (mEcuName.getVehicleSystem() << 1);
+
+	buffer[7] = (mEcuName.getVehicleSystemInstance() & VEHICLE_SYSTEM_INTERFACE_MASK) | ((mEcuName.getIndustryGroup() & INDUSTRY_GROUP_MASK) << 4) | ((mEcuName.isArbitraryAddressCapable() ? 1 : 0) << 7);
+
+}
+
+std::string AddressClaimFrame::toString() {
 
 	std::string retVal = J1939Frame::toString();
 	std::stringstream sstr;
 
-	sstr << "Id Number: " << mIdNumber << std::endl;
-	sstr << "Manufacturer Code: " << static_cast<u32>(mManufacturerCode) << std::endl;
-	sstr << "ECU Intance: " << static_cast<u32>(mEcuInstance) << std::endl;
-	sstr << "Function Instance: " << static_cast<u32>(mFunctionInstance) << std::endl;
-	sstr << "Function: " << static_cast<u32>(mFunction) << std::endl;
-	sstr << "Vehicle System: " << static_cast<u32>(mVehicleSystem) << std::endl;
-	sstr << "Vehicle System Instance: " << static_cast<u32>(mVehicleSystemInstance) << std::endl;
-	sstr << "Industry Group: " << static_cast<u32>(mIndustryGroup) << std::endl;
-	sstr << "Address Capable: " << (mArbitraryAddressCapable ? "yes" : "no") << std::endl;
+	sstr << "Id Number: " << mEcuName.getIdNumber() << std::endl;
+	sstr << "Manufacturer Code: " << static_cast<u32>(mEcuName.getManufacturerCode()) << std::endl;
+	sstr << "ECU Intance: " << static_cast<u32>(mEcuName.getEcuInstance()) << std::endl;
+	sstr << "Function Instance: " << static_cast<u32>(mEcuName.getFunctionInstance()) << std::endl;
+	sstr << "Function: " << static_cast<u32>(mEcuName.getFunction()) << std::endl;
+	sstr << "Vehicle System: " << static_cast<u32>(mEcuName.getVehicleSystem()) << std::endl;
+	sstr << "Vehicle System Instance: " << static_cast<u32>(mEcuName.getVehicleSystemInstance()) << std::endl;
+	sstr << "Industry Group: " << static_cast<u32>(mEcuName.getIndustryGroup()) << std::endl;
+	sstr << "Address Capable: " << (mEcuName.isArbitraryAddressCapable() ? "yes" : "no") << std::endl;
 
 	return retVal + sstr.str();
 
--- J1939/include/Addressing/AddressClaimFrame.h
+++ J1939/include/Addressing/AddressClaimFrame.h
@@ -10,14 +10,37 @@
 
 #include <J1939Frame.h>
 
+
 #define ADDRESS_CLAIM_PGN		0x00EE00
 #define ADDRESS_FRAME_LENGTH	8
+
+#define ARBITRARY_ADDR_CAPABLE_MASK		0x1
+#define ARBITRARY_ADDR_CAPABLE_OFFSET	63
+#define INDUSTRY_GROUP_MASK				0x7
+#define INDUSTRY_GROUP_OFFSET			60
+#define VEHICLE_SYSTEM_INTERFACE_MASK	0xF
+#define VEHICLE_SYSTEM_INTERFACE_OFFSET	56
+#define VEHICLE_SYSTEM_MASK				0x7F
+#define VEHICLE_SYSTEM_OFFSET			49
+#define FUNCTION_MASK					0xFF
+#define FUNCTION_OFFSET					40
+#define FUNCTION_INSTANCE_MASK			0x1F
+#define FUNCTION_INSTANCE_OFFSET		35
+#define ECU_INSTANCE_MASK				0x7
+#define ECU_INSTANCE_OFFSET				32
+#define MANUFACTURER_CODE_MASK			0x7FF
+#define MANUFACTURER_CODE_OFFSET		21
+#define IDENTITY_NUMBER_MASK			0x1FFFFF
+#define IDENTITY_NUMBER_OFFSET			0
+
 
 
 namespace J1939 {
 
-class AdressClaimFrame: public J1939Frame {
+
+class EcuName {
 private:
+
 	u32 mIdNumber = 0;
 	u16 mManufacturerCode = 0;
 	u8 mEcuInstance = 0;
@@ -28,23 +51,45 @@ private:
 	u8 mIndustryGroup = 0;
 	bool mArbitraryAddressCapable = false;
 
-protected:
-
-	void decodeData(const u8* buffer, size_t length);
-	void encodeData(u8* buffer, size_t length) const;
-
 public:
-	AdressClaimFrame();
-	AdressClaimFrame(u32 idNumber, u16 manufacturerCode, u8 ecuInstance,
+
+	EcuName() {}
+	EcuName(u32 idNumber, u16 manufacturerCode, u8 ecuInstance,
 			u8 functionInstance, u8 function, u8 vehicleSystem,
 			u8 vehicSystemInstance, u8 industryGroup,
-			bool arbitraryAddressCapable);
-	virtual ~AdressClaimFrame();
+			bool arbitraryAddressCapable) : mIdNumber(idNumber), mManufacturerCode(manufacturerCode),
+			mEcuInstance(ecuInstance), mFunctionInstance(functionInstance), mFunction(function),
+			mVehicleSystem(vehicleSystem), mVehicleSystemInstance(vehicSystemInstance), mIndustryGroup(industryGroup),
+			mArbitraryAddressCapable(arbitraryAddressCapable){
 
-	size_t getDataLength() const override { return ADDRESS_FRAME_LENGTH; }
+	}
+	virtual ~EcuName() {}
+
+	u64 getValue() const {
+
+		return ((u64)(mArbitraryAddressCapable ? ARBITRARY_ADDR_CAPABLE_MASK : 0) << ARBITRARY_ADDR_CAPABLE_OFFSET) |
+				((u64)(mIndustryGroup & INDUSTRY_GROUP_MASK) << INDUSTRY_GROUP_OFFSET) |
+				((u64)(mVehicleSystemInstance & VEHICLE_SYSTEM_INTERFACE_MASK) << VEHICLE_SYSTEM_INTERFACE_OFFSET) |
+				((u64)(mVehicleSystem & VEHICLE_SYSTEM_MASK) << VEHICLE_SYSTEM_OFFSET) |
+				((u64)(mFunction & FUNCTION_MASK) << FUNCTION_OFFSET) |
+				((u64)(mFunctionInstance & FUNCTION_INSTANCE_MASK) << FUNCTION_INSTANCE_OFFSET) |
+				((u64)(mEcuInstance & ECU_INSTANCE_MASK) << ECU_INSTANCE_OFFSET) |
+				((u64)(mManufacturerCode & MANUFACTURER_CODE_MASK) << MANUFACTURER_CODE_OFFSET) |
+				((u64)(mIdNumber & IDENTITY_NUMBER_MASK) << IDENTITY_NUMBER_OFFSET);
+
+	}
+
+	bool operator<(const EcuName& other) const {
+
+		return getValue() < other.getValue();
+	}
+
+	bool operator>(const EcuName& other) const {
+		return getValue() > other.getValue();
+	}
 
 	bool isArbitraryAddressCapable() const {
-		return mArbitraryAddressCapable;
+			return mArbitraryAddressCapable;
 	}
 
 	void setArbitraryAddressCapable(bool arbitraryAddressCapable) {
@@ -115,9 +160,28 @@ public:
 		mVehicleSystemInstance = vehicleSystemInstance;
 	}
 
+};
+
+class AddressClaimFrame: public J1939Frame {
+private:
+	EcuName mEcuName;
+protected:
+
+	void decodeData(const u8* buffer, size_t length);
+	void encodeData(u8* buffer, size_t length) const;
+
+public:
+	AddressClaimFrame();
+	AddressClaimFrame(EcuName name);
+	virtual ~AddressClaimFrame();
+
+	size_t getDataLength() const override { return ADDRESS_FRAME_LENGTH; }
+
+	const EcuName& getEcuName() { return mEcuName; }
+
 	std::string toString();
 
-	IMPLEMENT_CLONEABLE(J1939Frame,AdressClaimFrame);
+	IMPLEMENT_CLONEABLE(J1939Frame,AddressClaimFrame);
 
 };
 
