efeeea1e7dce433de2003210a6284802bed99996
famez <f.amez1992@gmail.com>
2019-05-23 16:41:28 -0400

    FIX: Bad encoding of DM1 frame in case of no DTCs

--- J1939/Diagnosis/Frames/DM1.cpp
+++ J1939/Diagnosis/Frames/DM1.cpp
@@ -5,8 +5,13 @@
  *      Author: famez
  */
 
+#include <string.h>
+
 #include <Diagnosis/Frames/DM1.h>
 #include <SPN/SPNStatus.h>
+#include <Utils.h>
+
+#define DM1_MINIMUM_LENGTH		8
 
 namespace J1939 {
 
@@ -62,6 +67,7 @@ DM1::~DM1() {
 
 void DM1::decodeData(const u8* buffer, size_t length) {
 
+
 	size_t lampStatLength = GenericFrame::getDataLength();
 
 	//Decode Lamp Status (SPNs)
@@ -75,7 +81,10 @@ void DM1::decodeData(const u8* buffer, size_t length) {
 
 		dtc.decode(buffer + offset);
 
-		mDtcs.push_back(dtc);
+		//To avoid adding a DTC when there are no faults (a DTC set all to 0s is sent which is not a valid DTC)
+		if(dtc.getSpn() != 0) {
+			mDtcs.push_back(dtc);
+		}
 
 		offset += DTC_SIZE;
 	}
@@ -84,12 +93,17 @@ void DM1::decodeData(const u8* buffer, size_t length) {
 
 void DM1::encodeData(u8* buffer, size_t length) const {
 
-
+	//Encode SPNs for bytes 0-1
 	size_t lampStatLength = GenericFrame::getDataLength();
 
 	GenericFrame::encodeData(buffer, lampStatLength);
 
-	size_t offset = lampStatLength;
+	size_t offset = lampStatLength;	//Must be 2
+
+	if(lampStatLength != 2) {
+		throw J1939EncodeException("[DM1::encodeData] SPNs are not expected to fit within "
+				"more than 2 bytes");
+	}
 
 	for(auto dtc = mDtcs.begin(); dtc != mDtcs.end(); ++dtc) {
 
@@ -99,11 +113,18 @@ void DM1::encodeData(u8* buffer, size_t length) const {
 
 	}
 
+	//According to the protocol, if there are no DTCs, we need to send the bytes 2-5 as 00 and bytes 6-7 as FF.
+	if(mDtcs.empty()) {
+		memset(buffer + offset, 0x00, 4);
+		memset(buffer + offset + 4, 0xFF, 2);
+	}
+
 }
 
 size_t DM1::getDataLength() const {
 
-	return GenericFrame::getDataLength() + DTC_SIZE * mDtcs.size();
+	//The length of DM1 frame is at least 8 bytes
+	return J1939_MAX(GenericFrame::getDataLength() + DTC_SIZE * mDtcs.size(), DM1_MINIMUM_LENGTH);
 
 }
 
